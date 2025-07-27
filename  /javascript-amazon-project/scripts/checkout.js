15abd99b9882534d67ccd79924572fe435918c85
SunilG <sunilbora1010@gmail.com>
2025-07-27 15:33:37 +0500

    Delete product when clicking delete

---  /javascript-amazon-project/scripts/checkout.js
+++  /javascript-amazon-project/scripts/checkout.js
@@ -14,7 +14,7 @@ cart.forEach((cartItem) => {
     }
   });
   cartSummaryHTML += `
-<div class="cart-item-container">
+<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
   <div class="delivery-date">Delivery date: Wednesday, June 15</div>
 
   <div class="cart-item-details-grid">
@@ -98,10 +98,13 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     //1.rmve product from cart
     const productId = link.dataset.productId;
-    console.log(productId);
     removeFromCart(productId);
-    console.log(cart);
 
     //2.update the HTML
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
