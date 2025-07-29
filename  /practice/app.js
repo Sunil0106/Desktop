541524cf497de2cccb8d25692eab63f529885bd3
SunilG <sunilbora1010@gmail.com>
2025-07-29 13:09:19 +0500

    boom

---  /practice/app.js
+++  /practice/app.js
@@ -1,13 +1,9 @@
-//Timing Event
-//The window object allows execution of code at specified time interval.These time intervals are called timing events
-//1.setTimeout(function, milliseconds)
-function hello() {
-  console.log("hello! khnana khais");
-}
-//* window.clearTimeout()
-//2.setInterval(function, milliseconds):repeats a given function at every given time-interval
-setInterval(timer, 1000);
-function timer() {
+//JavaScript Cookies
+//-are data stored in small text files on your computer;
+function setcookie(cname, cvalue, exdays){
   const d = new Date();
-  document.getElementById("date").innerHTML = d;
+  d.setTime(d.getTime()+ (exdays*24*69*60*1000))
+
+  let expires ="expires ="+d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ',' expires + "; path"
 }
