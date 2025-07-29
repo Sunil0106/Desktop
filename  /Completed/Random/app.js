325b3b4a7c482ae7a8e421fdd90ccf4f3b448324
SunilG <sunilbora1010@gmail.com>
2025-07-29 09:19:43 +0500

    bad bad

---  /Completed/Random/app.js
+++  /Completed/Random/app.js
@@ -1,12 +1,12 @@
-const foodName = "tea";
-const apiKey = " 7530d71bbc724890ac768ebb6501cb5e ";
+const foodName = "chicken";
+const apiKey = " 7530d71bbc724890ac768ebb6501cb5e";
 
-const method = {
-  method: "GET",
-  headers: {
-    "X-Api-Key": apiKey,
-  },
-};
+// const method = {
+//   method: "GET",
+//   headers: {
+//     "X-Api-Key": apiKey,
+//   },
+// };
 //then method
 // fetch(url, method)
 //   .then((res) => {
@@ -22,9 +22,21 @@ const method = {
 //async await
 async function getDataApi(foodName) {
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`;
-  const response = await fetch(url, method);
+  const response = await fetch(url);
   const data = await response.json();
   console.log(data);
+  // const foodImage = await getFoodImage(foodName);
+  // console.log(foodImage);
 }
 
-getDataApi("chicken");
+// getDataApi(foodName);
+
+// async function getFoodImage(foodName) {
+//   const url = `https://foodish-api.herokuapp.com/images/${foodName}/`;
+
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   console.log(data);
+// }
+
+// getFoodImage(foodName);
