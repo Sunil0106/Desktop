00bfc84dd3bbdae70f2d258c285714a1785cfde2
SunilG <sunilbora1010@gmail.com>
2025-07-30 10:44:49 +0500

    Create automated test for formatCurrency

---
+++  /javascript-amazon-project/tests/moneyTest.js
@@ -0,0 +1,39 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+// test cases
+//1.basic test cases
+//2.Edge cases
+
+//basic test case
+console.log("Test suite:  formatCurrency");
+console.log("converts cents into dollars");
+if (formatCurrency(2095) == "20.95") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+//edge cases
+console.log("works with 0");
+if (formatCurrency(0) === "0.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("round ups to the nearest cent");
+if (formatCurrency(2000.5) === "20.01") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+//test
+//1.mannual test: manually
+//disadvantage
+//a.Hard to test every situation
+//b.Hard to re-test
+
+//2.automated test: use code to test code;
+
+//test suite a group of related test
