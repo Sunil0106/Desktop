bb6292f1d0eccbe838f3056497ad74c33f0c2d67
SunilG <sunilbora1010@gmail.com>
2025-08-01 15:50:31 +0500

    pushpa

---  /Completed/facebook/JavaScript/accountcreate.js
+++
@@ -1,8 +0,0 @@
-const fnameEl = document.getElementById("fname");
-const lnameEl = document.getElementById("lname");
-console.log(lnameEl);
-
-function checkName(first, last) {
-  if (!first || !last) {
-  }
-}
